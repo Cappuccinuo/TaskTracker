5771ddb133063bf9de0fb3b428193ac713a3d1a0
Yuan Gao <gaoyuanbj@yahoo.com>
2018-02-14 19:59:39 -0500

    Fix the phoenix default page

--- tasktracker/config/dev.exs
+++ tasktracker/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :tasktracker, Tasktracker.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "task",
+  password: "123456",
   database: "tasktracker_dev",
   hostname: "localhost",
   pool_size: 10
